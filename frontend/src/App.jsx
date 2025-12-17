cbe6382ad63f1594aba36f35c5d164d121f712f8
Vaibhav Sutar <sutar.vaibhav649@gmail.com>
2025-12-17 17:33:10 +0530

    frontend setup completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h3>Student Task Manager</h3>
+        <h4>Frontend setup</h4>
+      </div>        
+    </>
+  )
+}
+
+export default App
