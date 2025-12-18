c73573ac00ee0518e503d4e1ff280f90d981fd10
Vaibhav Sutar <sutar.vaibhav649@gmail.com>
2025-12-18 21:01:10 +0530

    Login and register page created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,56 @@
 import React from 'react'
-
+import {Link} from "react-router-dom";
 const Login = () => {
   return (
-    <>
-        <div className=' bg-gray-200'>
-            <h1 className=' text-2xl text-blue-500'>
-                Login Page
-            </h1>
+    // outer div
+      <div className=' min-h-screen flex justify-center items-center bg-gray-900'>
+        <div className=' w-full max-w-md bg-gray-800 rounded-xl shadow-lg p-8'>
+          <h1 className=' text-2xl font-bold text-center text-white mb-6'>
+            Login
+          </h1>
+          {/* ================= login form starts ================ */}
+          <form className=' space-y-4'>
+            <div>
+              {/* ========= Email input starts ========== */}
+              <label className=' block text-sm text-gray-300 mb-1'>
+                Email
+              </label>
+              <input 
+                type="email"
+                placeholder='you@example.com'
+                className='w-full px-4 py-2 rounded-lg bg-gray-700 text-white outline-none focus:ring-2 focus:ring-blue-500'
+              />
+              {/* ========= Email input Ends ========== */}
+              {/* ========= Password input starts ========== */}
+              <label className='block text-sm text-gray-300 mb-1'>
+                Password
+              </label>
+              <input
+                type='password'
+                placeholder='**********'
+                className='w-full px-4 py-2 rounded-lg bg-gray-700 text-white outline-none focus:ring-2 focus:ring-blue-500'
+              />
+              {/* ========= Password input Ends ========== */}
+            </div>
+            {/* =========== Login Button starts =========== */}
+            <button
+              type='submit' 
+              className=' w-full bg-blue-500 hover:bg-blue-700 text-white py-2 rounded-lg font-semibold transition hover:cursor-pointer'           
+            >  
+            Login
+            </button>
+          
+            {/* =========== Login Button ends =========== */}
+          </form>
+          {/* ================= login form ends ================ */}
+          <p className='text-sm text-gray-400 text-center mt-4'>
+            Don't have an account?{" "}
+            <Link to="/register" className='text-blue-400 hover:underline'>
+              Register
+            </Link>
+          </p>
         </div>
-    </>
+      </div>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,66 @@
 import React from 'react'
-
+import {Link} from "react-router-dom";
 const Register = () => {
   return (
-    <>
-        <div className=' bg-gray-200'>
-            <h1 className=' text-2xl text-blue-500'>
-                Register Page
-            </h1>
+    // outer div
+      <div className=' min-h-screen flex justify-center items-center bg-gray-900'>
+        <div className=' w-full max-w-md bg-gray-800 rounded-xl shadow-lg p-8'>
+          <h1 className=' text-2xl font-bold text-center text-white mb-6'>
+            Register
+          </h1>
+          {/* ================= login form starts ================ */}
+          <form className=' space-y-4'>
+            <div>
+              {/* ========= Name input starts ========== */}
+              <label className=' block text-sm text-gray-300 mb-1'>
+                Name
+              </label>
+              <input 
+                type="text"
+                placeholder='Vaibhav Sutar'
+                className='w-full px-4 py-2 rounded-lg bg-gray-700 text-white outline-none focus:ring-2 focus:ring-blue-500'
+              />
+              {/* ========= Name input Ends ========== */}
+              {/* ========= Email input starts ========== */}
+              <label className=' block text-sm text-gray-300 mb-1'>
+                Email
+              </label>
+              <input 
+                type="email"
+                placeholder='you@example.com'
+                className='w-full px-4 py-2 rounded-lg bg-gray-700 text-white outline-none focus:ring-2 focus:ring-blue-500'
+              />
+              {/* ========= Email input Ends ========== */}
+              {/* ========= Password input starts ========== */}
+              <label className='block text-sm text-gray-300 mb-1'>
+                Password
+              </label>
+              <input
+                type='password'
+                placeholder='**********'
+                className='w-full px-4 py-2 rounded-lg bg-gray-700 text-white outline-none focus:ring-2 focus:ring-blue-500'
+              />
+              {/* ========= Password input Ends ========== */}
+            </div>
+            {/* =========== Login Button starts =========== */}
+            <button
+              type='submit' 
+              className=' w-full bg-blue-500 hover:bg-blue-700 text-white py-2 rounded-lg font-semibold transition hover:cursor-pointer'           
+            >  
+            Login
+            </button>
+          
+            {/* =========== Login Button ends =========== */}
+          </form>
+          {/* ================= login form ends ================ */}
+          <p className='text-sm text-gray-400 text-center mt-4'>
+            Have an account?{" "}
+            <Link to="/login" className='text-blue-400 hover:underline'>
+              Login
+            </Link>
+          </p>
         </div>
-    </>
+      </div>
   )
 }
 
